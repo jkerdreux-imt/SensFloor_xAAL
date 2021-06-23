96954d07f0a2a5ddfd620e2307d10d57f4e80a54
Mahé <mahe.qu56@gmail.com>
2021-06-23 11:17:44 +0200

    mis à jour

--- xaal_sensfloor.py
+++ xaal_sensfloor.py
@@ -2,6 +2,7 @@ from gevent import monkey; monkey.patch_all()
 
 from xaal.lib import tools,Device
 from xaal.lib.asyncio import Engine
+import threading
 import time
 import socketio
 import asyncio
@@ -16,8 +17,8 @@ PACKAGE_NAME = 'xaal.SensFloor'
 logger = logging.getLogger(PACKAGE_NAME)
 FUNCTION_PRESENCE = 'Presence'
 FUNCTION_FALL = 'Fall'
+ATTTRIBUTE_TIME = 'Delay (s) '
 ip_addr = 'http://172.25.0.178:8000'
-#ip_addr = 'http://192.168.5.5:8000'
 
 
 class Socketio_class(object):
@@ -27,6 +28,7 @@ class Socketio_class(object):
     def __init__(self):
         self.project_installation = False
         self.device_installation = False
+        self.fall_previous_state = {}
         self.sio = socketio.AsyncClient()
         self.config()
         self.data_areas = {} # Contient l'id de l'application ainsi que son nom
@@ -37,8 +39,10 @@ class Socketio_class(object):
         self.sio.on('connect', self.connect)    
         self.sio.on('funcresult-update', self.etat_app)
         self.sio.on('project', self.project)
+        self.sio.on('fall',self.coord_fall_detection)
         self.eng = Engine()
         self.devices = {}
+        self.time_fall = {}
         atexit.register(self._exit)
 
     async def connect(self):
@@ -54,12 +58,42 @@ class Socketio_class(object):
                 self.zones_presence[cle]=self.data_areas[cle]
             if (self.functs[cle]==FUNCTION_FALL and not(self.data_areas[cle] in self.zones_fall)):
                 self.zones_fall[cle]=self.data_areas[cle]
+                self.fall_previous_state[cle]=False
+
         self.project_installation = True
 
     async def etat_app(self,msg):
         if self.device_installation == True :
             self.treatment_msg(msg)
-            
+
+    async def coord_fall_detection (self,msg):
+        if self.device_installation == True :
+            if (self.fall_previous_state[msg['id']]!= msg['result']):
+                self.fall_previous_state[msg['id']] = msg['result']
+                dev=self.l[msg["id"]]
+                attr_x=dev.get_attribute("X")
+                attr_y=dev.get_attribute("Y")
+                attr_time=dev.get_attribute(ATTTRIBUTE_TIME)
+                if (len(self.time_fall) == 0):
+                    thread = Delay(dev)
+                    self.time_fall[dev]=thread
+                else :
+                    if (dev in self.time_fall):
+                        thread = self.time_fall[dev]
+                    else :
+                        thread = Delay(dev)
+                        self.time_fall[dev]=thread
+                if msg['result']== True:
+                    thread.start()
+                    attr_x.value=msg['fall_center'][0]['x']
+                    attr_y.value=msg['fall_center'][0]['y']
+                if msg['result']==False :
+                    del self.time_fall[dev]
+                    thread.stop()
+                    attr_x.value="None"
+                    attr_y.value="None"    
+                print("LISTE : ",self.time_fall)
+          
     def treatment_msg(self,message):
         if not(len(self.l)==0):
             if (self.functs[message["uid"]]==FUNCTION_PRESENCE or self.functs[message["uid"]]==FUNCTION_FALL):
@@ -115,14 +149,20 @@ class Socketio_class(object):
                         dev.address=base_addr
 
                 fall = dev.new_attribute('Fall')
-                fall.value = "init"
+                coord_x = dev.new_attribute('X')
+                coord_y = dev.new_attribute('Y')
+                delay = dev.new_attribute(ATTTRIBUTE_TIME)
+                fall.value = "None"
+                coord_x.value = "None"
+                coord_y.value = "None"
+                delay.value = "None"
                 dev.dump()
                 self.l[id]=dev
                 addr_list.append(dev.address)
                 self.eng.add_device(dev)
                 i=i+1
 
-
+        i=0
         if (len(self.zones_presence)!=0):   
             for id,name in self.zones_presence.items():
                 name_basic="presence.basic"
@@ -169,6 +209,29 @@ class Socketio_class(object):
         if cfg != self.cfg:
             logger.info('Saving configuration file')
             self.cfg.write()
+
+class Delay (threading.Thread):
+    def __init__(self, dev):
+        threading.Thread.__init__(self)
+        self.dev=dev
+        self.attr_time=self.dev.get_attribute(ATTTRIBUTE_TIME)
+        self.Terminated = False
+        self.st=0
+        self.end=0
+
+    def run(self):
+        self.st=time.time()
+        while not self.Terminated:
+            time.sleep(1)
+            self.mesure()
+        self.attr_time.value=0 
+
+    def mesure(self):
+        time_spent=time.time()-self.st
+        self.attr_time.value=int(time_spent)
+
+    def stop(self):
+        self.Terminated = True   
 
 if __name__ == '__main__':
     logger.info('Starting %s' % PACKAGE_NAME)
